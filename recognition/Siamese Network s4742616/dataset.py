c5acacbebb9786ef26bd9fc082db05a374406be8
Philip <p.burke@uq.edu.au>
2025-10-29 22:35:46 +1000

    Finished dataset preprocessing in dataset.py and train.py

--- recognition/Siamese Network s4742616/dataset.py
+++ recognition/Siamese Network s4742616/dataset.py
@@ -1,16 +1,18 @@
 import torch
 import numpy as np
 import pandas as pd
+from torchvision import transforms
 import os
-
+from PIL import Image
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split
 
 #Dataset sources
-Y_DATA = "data\ISIC_2020_Training_GroundTruth.csv" #Y data source
-X_DATA = "data\train" #X data source
+Y_DATA = "PatternAnalysis-2025/recognition/Siamese Network s4742616/data/ISIC_2020_Training_GroundTruth.csv" #Y data source
+X_DATA = "PatternAnalysis-2025/recognition/Siamese Network s4742616/data/train/" #X data source
 
 #Temp File Names
-FILENAME_X = "train.pt"
-#FILENAME_Y = "train.py"
+FILENAME_X = "PatternAnalysis-2025/recognition/Siamese Network s4742616/train2.pt"
 
 #Some default parameters
 DIMENSIONS = 256 #Dimensions of resized image
@@ -23,7 +25,23 @@ def preprocess_X(device = "cpu", X_source = X_DATA, newSize = DIMENSIONS):
         2.Resize to square (dim * dim)
         3.Data augmentation (Possibly)
         Returns filename of tensor saved to file """
+    transform = transforms.Compose([
+        transforms.Resize((DIMENSIONS, DIMENSIONS)),
+        transforms.ToTensor(),
+    ])
+
+    images = []
+
+    for filename in tqdm(os.listdir(X_DATA), desc = "Processing Images"):
+        if filename.lower().endswith('.jpg'): #It will
+            image_path = os.path.join(X_DATA, filename)
+            image = Image.open(image_path).convert("RGB") #Keeps colours
+            image = transform(image) #Convert to tensor, resize
+            images.append(image)
     
+    X = torch.stack(images) #Converts to single tensor
+
+    torch.save(X, FILENAME_X)
     
 
     return FILENAME_X
@@ -41,3 +59,21 @@ def preproccess_Y(device = "cpu", Y_source = Y_DATA):
 
     return y
 
+def train_test_valid_split(X, Y):
+    indices = list(range(len(X))) #indices of X data
+    #Split 70% of the data into training set
+    train_idx, temp_idx, train_labels, temp_labels = train_test_split(indices, Y, test_size = 0.3, stratify = Y)
+    #Split 20% of overall data into test set, 10% into validation
+    val_idx, test_idx, val_labels, test_labels = train_test_split(temp_idx, temp_labels, test_size = 0.66, stratify = temp_labels)
+
+
+    x_train = X[train_idx]
+    y_train = train_labels
+
+    x_val = X[val_idx]
+    y_val = val_labels
+
+    x_test = X[test_idx]
+    y_test = test_labels
+
+    return x_train, y_train, x_val, y_val, x_test, y_test
--- recognition/Siamese Network s4742616/train.py
+++ recognition/Siamese Network s4742616/train.py
@@ -2,18 +2,35 @@ from dataset import *
 import torch
 import os
 
+
+
 RANDOM_STATE = 354
 
 
+PRE_PROCCESSED = True #Flag to see if we've already pre-processed data 
+X_PATH = "PatternAnalysis-2025/recognition/Siamese Network s4742616/train.pt"
+
 if __name__ == "__main__":
-    device = torch.device("cuda" if torch.cude_is_available() else "cpu")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     #Loads and preproccesses datasets
     print(f"Training on Device: {device}")
     Y = preproccess_Y(device)
-    print(Y[1]) #Test, please remove!
-    X_name = preprocess_X(device, type)
-    print(X_name) #Test, please remove!
+    if PRE_PROCCESSED and os.path.exists(X_PATH):
+        X_name = X_PATH
+    else:
+        X_name = preprocess_X(device)
     X = torch.load(X_name)
 
-    #Splits data into train and test splits
+    #Splits data into train, test and validation splits
+    x_train, y_train, x_val, y_val, x_test, y_test = train_test_valid_split(X,Y)
+
+    x_test, y_test = 0,0 #Don't need them for training
+
+
+
+    #load models
+
+    #begin training loop
+
+    #save model, maybe both? 
 
